5f91ea6144c3bad1cfeefd8e6282dc992399e324
Hari <harikn77@gmail.com>
2023-11-19 15:47:12 +0530

    Add request body validation

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,6 +1,14 @@
-const ingestLog = (req, res) => {
-    const logData = req.body;
+const { validationResult } = require('express-validator');
 
+const ingestLog = (req, res) => {
+    // Validate JSON format
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const logData = req.body;
+    console.log(logData);
     // TODO: Implement log ingestion logic (e.g., store in a database)
 
     console.log('Received log:', logData);
@@ -8,5 +16,5 @@ const ingestLog = (req, res) => {
 };
 
 module.exports = {
-    ingestLog,
-};
+    ingestLog
+}
--- routes/logRoutes.js
+++ routes/logRoutes.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 const logController = require('../controllers/logController');
 
+// Validation rules for log data
+const logValidationRules = [
+    body('level').optional().isString(),
+    body('message').isString(),
+    body('resourceId').optional().isString(),
+    body('timestamp').optional().isISO8601(),
+    body('traceId').optional().isString(),
+    body('spanId').optional().isString(),
+    body('commit').optional().isString(),
+    body('metadata.optional().parentResourceId').optional().isString(),
+];
+
 // Endpoint to receive logs via HTTP POST
-router.post('/ingest', logController.ingestLog);
+router.post('/ingest', logValidationRules, logController.ingestLog);
 
 module.exports = router;
